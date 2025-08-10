197e15f9a71a57966d3f8893dd6e436a9be4d2cd
clampris <clampris@gmail.com>
2025-08-10 21:33:15 +0800

    js/09/add function in sumAll.js and updated spec

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    // Check for errors
+    if (min < 0 || max < 0 || typeof min !== "number" || typeof max !== "number" || !Number.isInteger(max) || !Number.isInteger(min)) {
+        return 'ERROR';
+    }
+    // Swap values if min is greater than max
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

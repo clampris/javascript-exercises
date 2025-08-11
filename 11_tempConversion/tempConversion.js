6343611e4b5dc4c8dab2f0fa703f1d53767a76ec
clampris <clampris@gmail.com>
2025-08-11 22:55:20 +0800

    js/11/add function in tempConversion.js and update spec

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp_f) {
+  return parseFloat(((temp_f - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_c) {
+  return parseFloat(((temp_c * 9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line

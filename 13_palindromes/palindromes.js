4626ba79c16c6c76fc6ad5cda265ff647bd9b565
clampris <clampris@gmail.com>
2025-08-24 15:57:45 +0800

    js/13/added function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const lowerCase = word.toLowerCase().match(/[a-z0-9]/g);
+    const reversed = lowerCase.slice().reverse().join("");
+    return lowerCase.join("") === reversed;
 };
 
 // Do not edit below this line

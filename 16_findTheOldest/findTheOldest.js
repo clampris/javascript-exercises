c4c44034916ff600e0aecbb0730a7a2824b9c223
clampris <clampris@gmail.com>
2025-09-05 23:09:46 +0800

    js/16/add function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(peopleList) {
+    const currentYear = new Date().getFullYear();
+    return peopleList.reduce((oldest, person) => {
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        return oldestAge < personAge ? person : oldest;
+    });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
